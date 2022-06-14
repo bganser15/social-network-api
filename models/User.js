19b695574821b54667c0b8dc4d7763fee1fe6d74
Brittany Ganser <15brganser@gmail.com>
2022-06-13 21:09:22 -0600

    ability to create a new user

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    thoughts: [],
+    friends: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+const User = model("User", UserSchema);
+
+module.exports = User;
