741c8be1a93a29e6d921ba5892cdfc0e8f107a06
Brittany Ganser <15brganser@gmail.com>
2022-06-15 23:07:36 -0600

    get user by ID

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,12 +13,32 @@ const userController = {
     User.find({})
       //displays thoughts
       .populate({
-      path: "thoughts",
-      select: "-__v",
+        path: "thoughts",
+        select: "-__v",
       })
       .select("-__v")
       .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //find user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 
-const { createUser, getAllUser } = require("../../controllers/user-controller");
+const {
+  createUser,
+  getAllUser,
+  getUserById,
+} = require("../../controllers/user-controller");
 
 // /api/user
 router.route("/").post(createUser).get(getAllUser);
+
+// api/user/:id
+router.route("/:id").get(getUserById);
 
 module.exports = router;
